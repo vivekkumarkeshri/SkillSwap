bdb2ebe7bdce0678a347499ca10370f00c384155
Vivek Kumar <vivekk0479@gmail.com>
2026-05-23 23:55:09 +0530

    remove duplicate folders

--- Course-Selling-Application-in-MERN-main/backend/src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
-});
-
-module.exports = mongoose.model("User", userSchema);
--- Course-Selling-Application-in-MERN-main/frontend/src/components/GlobalState.js
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil";
-
-export const loginState = atom({
-    key: 'loginState',
-    default: {
-        user: false,
-        admin: false
-    }
-  });
